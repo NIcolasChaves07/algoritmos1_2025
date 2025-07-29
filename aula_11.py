ee180a9ad2912c6e02ee80ae6e5652b713cbb625
unknown <nicolaschaves2921@gmail.com>
2025-07-29 19:29:28 -0300

    Third commit

---
+++ aula_11.py
@@ -0,0 +1,8 @@
+nome = [""] * 5
+
+nome[0] = "Rafael"
+nome[1] = "Ayslan"
+nome[2] = "Daniela"
+nome[3] = "Frank"
+
+print(nome[0], nome[1], nome[2], nome[3])
---
+++ aula_12.py
@@ -0,0 +1,13 @@
+#Exercício 2: Percorrendo um vetor com laço de repetição em ordem decrescente
+
+# lista = [0] * 100
+
+# for i in range(len(lista), -1, -1):
+#     print(f"Numero: {i}")
+
+#Exercicío 2: Prercorrendo um vetor em um laço de repetição em ordem crescente
+lista = [0] * 100
+
+# for i in range(0, len(lista)):
+#     lista[i] = lista[i] + 1
+#     print(f"Número: {i}")
